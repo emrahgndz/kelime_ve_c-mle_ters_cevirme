5db57b68366f7f0c24c8460660646cc0da5f4126
Emrah Gündüz <75933217+emrahgndz@users.noreply.github.com>
2022-06-09 18:53:06 +0300

    kelime_ve_c-mle_ters_cevirme

---
+++ kelime_ve_cümle_ters_cevirme.py
@@ -0,0 +1,11 @@
+a = "Merhaba"
+print(a)
+print("Tersten yazımı = {}".format(a[::-1]))
+
+
+b= "YAŞA MUSTAFA KEMAL PAŞA "
+print(b)
+print("{}".format(b[::-1]))
+
+c = input("Bir kelime giriniz: ")
+print(c[::-1])
